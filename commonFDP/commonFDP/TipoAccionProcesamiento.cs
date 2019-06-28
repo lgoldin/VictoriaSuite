c81b63c9d71f74117fcd8c9aa86a97466237eeaf
julian iervasi <julian.iervasi@hotmail.com>
2019-06-27 20:59:49 -0700

    subida de common fdp

---
+++ commonFDP/commonFDP/TipoAccionProcesamiento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace commonFDP
+{
+    public static class commonFDP
+    {
+        public  enum TipoAccionProcesamiento
+        {
+            AGREGAR_REGISTRO,
+            MODIFICAR_REGISTRO,
+            BORRAR_SELECCIONADOS,
+            SELECCIONAR_TODOS,
+            FILTRAR
+        }
+    }
+}
+
+
+
